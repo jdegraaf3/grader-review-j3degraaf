50f7793e1b27b9b10728a53002b13f956a9febd2
jdegraaf3 <jdegraaf@ucsd.edu>
2023-05-25 15:17:36 -0700

    changes

--- grade.sh
+++ grade.sh
@@ -8,6 +8,35 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo ListExamples.java Submitted
+    cp student-submission/ListExamples.java grading-area
+    cp TestListExamples.java grading-area
+else
+    echo ListExamples.java NOT Submitted
+    exit 1
+fi
+
+javac -cp $CPATH grading-area/*.java
+
+if [[ $? -eq 0 ]]
+then
+    echo Succesful Compilation
+else
+    echo "file couldn't compile 0/4"
+    exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JunitCore TestListExamples > junit-output.txt
+
+#FAILURES = 'grep'
+
+#if [[ $FAILURES -eq 0 ]]
+#then
+#    echo All Tests Passed
+#    echo '4/4'
+#else
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
